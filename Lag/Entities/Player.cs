612a2ac024b77d88c7e55764b1791514bf910f2a
David Prior <david.prior@davecheesefish.com>
2016-02-14 00:34:38 +0000

    Added Player class.

---
+++ Lag/Entities/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Lag.Entities
+{
+    class Player
+    {
+        public Vector2 Position { get { return position; } }
+        private Vector2 position;
+
+        private Texture2D texture;
+
+        public Player(Vector2 position)
+        {
+            this.position = position;
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+            texture = content.Load<Texture2D>("player");
+        }
+
+        internal void Update(GameTime gameTime)
+        {
+            
+        }
+
+        internal void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, null, Color.White, 0.0f, new Vector2(60.0f, 60.0f), 1.0f, SpriteEffects.None, 0.0f);
+        }
+    }
+}
--- Lag/Screens/LevelScreen.cs
+++ Lag/Screens/LevelScreen.cs
@@ -5,6 +5,7 @@ using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using Lag.Entities;
 
 namespace Lag.Screens
 {
@@ -13,19 +14,33 @@ namespace Lag.Screens
     /// </summary>
     class LevelScreen:Screen
     {
+        /// <summary>
+        /// Saved reference to the content manager for loading and retrieving entity content.
+        /// </summary>
+        private ContentManager contentManager;
+
+        private Player player;
+
+        public LevelScreen()
+            : base()
+        {
+            player = new Player(new Vector2(400, 300));
+        }
+
         public override void LoadContent(ContentManager contentManager)
         {
-            // TODO: Load content here
+            this.contentManager = contentManager;
+            player.LoadContent(contentManager);
         }
 
         public override void Update(GameTime gameTime)
         {
-            // TODO: Update here
+            player.Update(gameTime);
         }
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-            // TODO: Draw here
+            player.Draw(gameTime, spriteBatch);
         }
     }
 }
