434c56659943dd07d1495771b4a2542ccc5f4c3d
David Prior <david.prior@davecheesefish.com>
2016-03-21 12:16:57 +0000

    Added credits screen.

---
+++ Lag/Screens/CreditsScreen.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Lag.Screens
+{
+    class CreditsScreen:Screen
+    {
+        private Texture2D texture;
+
+        public CreditsScreen(ScreenManager manager)
+            : base(manager)
+        {
+        }
+
+        public override void LoadContent(ContentManager contentManager)
+        {
+            texture = contentManager.Load<Texture2D>("credits");
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (InputManager.Instance.KeyReleased(Keys.Enter))
+            {
+                // If enter is pressed, go back to the main menu.
+                manager.GoTo(new MenuScreen(manager));
+            }
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            // Draw the credits texture.
+            spriteBatch.Draw(texture, Vector2.Zero, Color.White);
+        }
+    }
+}
--- Lag/Screens/MenuScreen.cs
+++ Lag/Screens/MenuScreen.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -19,7 +15,7 @@ namespace Lag.Screens
         /// <summary>
         /// Menu option strings.
         /// </summary>
-        private string[] menuItems = { "Play game" };
+        private string[] menuItems = { "Play game", "Credits" };
 
         /// <summary>
         /// Text colour for the active menu item.
@@ -135,6 +131,9 @@ namespace Lag.Screens
             {
                 case 0: // Play game
                     manager.GoTo(new LevelScreen(manager));
+                    break;
+                case 1: // Credits
+                    manager.GoTo(new CreditsScreen(manager));
                     break;
             }
         }
